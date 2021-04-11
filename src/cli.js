9b4c7ae49e6ed73f4a55f41b6b9a2320d9446181
Yan-Jobs <yan@yanjobs.me>
2021-04-11 11:56:58 +0200

    Prompt for options created but problem with the default name, fixed later today

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,65 @@
-export function cli(args) {
-    console.log("Hi ! How are you ?\nI finish this cli this week-end. Don't worry about that.");
+import arg from 'arg';
+import inquirer from "inquirer";
+
+function parseArgumentsIntoOptions(rawArgs) {
+    const args = arg(
+        {
+            "--git": Boolean,
+            "--yes": Boolean,
+            "-g": "--git",
+            "-y": "--yes",
+        },
+        {
+            argv: rawArgs.slice(2),
+        }
+    );
+    return {
+        skipPrompts: args["--yes"] || false,
+        git: args["--git"] || false,
+        projectName: args._[0],
+        template: args._[1],
+    }
+}
+
+async function promptForMissingOptions(options) {
+    const defaultTemplate = "Website with HTML,";
+    const defaultName = "your_project";
+    if (options.projectName) {
+        return {
+            ...options,
+            projectName: options.projectName || defaultName,
+        };
+    }
+    const questions = [];
+    if (!options.template) {
+        questions.push({
+            type: 'list',
+            name: 'template',
+            message: "Please choose what do you want to do today",
+            choices: ["Website with HTML,", "A discord Bot with discord.js"],
+            default: defaultTemplate,
+        });
+    }
+
+    if (!options.git) {
+        questions.push({
+            type: "confirm",
+            name: "git",
+            message: "Initialize a git repository ?",
+            default: false,
+        });
+    }
+
+    const answers = await inquirer.prompt(questions);
+    return {
+        ...options,
+        template: options.template || answers.template,
+        git: options.git || answers.git,
+    };
+}
+
+export async function cli(args) {
+    let options = parseArgumentsIntoOptions(args);
+    options = await promptForMissingOptions(options);
+    console.log(options);
 }
